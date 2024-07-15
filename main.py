eb8d6ad6f7aeb8c9c98922f75a61e750ee53e182
Gresh Basic <100721569+greshbasic@users.noreply.github.com>
2024-07-14 19:35:51 -0500

    clarifying predict()

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from prediction import predict
 
 if __name__ == "__main__":
     new_model = model.create_model()
+
+    # If you'd like you can import your own photos and have the model
+    # take its guess at them :)
+    # here is a quick example of how I went about it
     
-    print("which character do you want to predict?")
-    print("1. Luffy")
-    print("2. Sanji")
-    print("3. Law")
-    print("4. Chopper")
-    print("5. Exit")
-    choice = int(input("Enter your choice: "))    
+    # print("which character do you want to predict?")
+    # print("1. Luffy")
+    # print("2. Sanji")
+    # print("3. Law")
+    # print("4. Chopper")
+    # print("5. Exit")
+    # choice = int(input("Enter your choice: "))    
     
-    predict(new_model, choice)
+    # predict(new_model, choice)
